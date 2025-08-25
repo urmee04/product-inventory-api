df4f7bef17672aaca451dd0c79cbe134ccfd4e7b
urmee04 <urmee04sust@gmail.com>
2025-08-25 15:24:33 -0400

    add POST /api/products endpoint with error implementation

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,3 +2,21 @@
 const express = require("express");
 const router = express.Router();
 const Book = require("../models/Product");
+const Product = require("../models/Product");
+
+//POST / - Creates a new product using the data in req.body
+
+router.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    //return the product directly
+    res.status(201).json(product);
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return res.status(400).json({ error: error.message });
+    }
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+module.exports = router;
